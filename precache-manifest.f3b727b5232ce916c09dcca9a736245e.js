8baa24313c5494513ff6f3f28443709f216aaaf9
Your Name <you@example.com>
2020-07-07 12:02:43 +0545

    Updates

---
+++ precache-manifest.f3b727b5232ce916c09dcca9a736245e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51065830847af36508b6ea8c0808a109",
+    "url": "/React-Gallery-and-Table-Library/index.html"
+  },
+  {
+    "revision": "1e53f99fb31dc6bd24c1",
+    "url": "/React-Gallery-and-Table-Library/static/css/2.63d86ec7.chunk.css"
+  },
+  {
+    "revision": "84ef8c427746bb05c8db",
+    "url": "/React-Gallery-and-Table-Library/static/css/main.140d9af6.chunk.css"
+  },
+  {
+    "revision": "1e53f99fb31dc6bd24c1",
+    "url": "/React-Gallery-and-Table-Library/static/js/2.686d9f34.chunk.js"
+  },
+  {
+    "revision": "1936c9aa17248899125200b8b74c409d",
+    "url": "/React-Gallery-and-Table-Library/static/js/2.686d9f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84ef8c427746bb05c8db",
+    "url": "/React-Gallery-and-Table-Library/static/js/main.d5e183db.chunk.js"
+  },
+  {
+    "revision": "258c4e5a16a4840d9f12",
+    "url": "/React-Gallery-and-Table-Library/static/js/runtime-main.284df04d.js"
+  }
+]);
